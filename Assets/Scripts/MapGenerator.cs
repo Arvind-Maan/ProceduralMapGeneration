d3e032754e23cbd0e0ff1a7fe0554adf35a5e309
Arvind-Maan <arvindmaan1199@gmail.com>
2019-10-14 19:54:31 -0500

    Added World Seeds

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,15 +7,34 @@ public class MapGenerator : MonoBehaviour
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
+
     public int octaves;
+    [Range(0,1)]
     public float persistance;
     public float lacunarity;
+
     public bool autoUpdate;
+
+    public int seed;
+    public Vector2 offset;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance,lacunarity);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance,lacunarity, offset);
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawMap(noiseMap);
+    }
+
+    //called automatically when a script variable is changed
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+            mapWidth = 1;
+        if (mapHeight < 1)
+            mapHeight = 1;
+        if (lacunarity < 1)
+            lacunarity = 1;
+        if (octaves < 0)
+            octaves = 0;
     }
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
-        if(scale <= 0) //if the scale is less than 1, put it as a default value of 0.0001f
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float xOffset = prng.Next(-100000, 100000) + offset.x;
+            float yOffset = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(xOffset, yOffset);
+        } //end of for-loop
+
+        if (scale <= 0) //if the scale is less than 1, put it as a default value of 0.0001f
             scale = 0.0001f;
 
         float maxNHeight = float.MinValue;
         float minNHeight = float.MaxValue;
+        //we calculate the values of the middle of the screen so when we change our noise scale, it will be to the center.
+        float middleX = mapWidth / 2;
+        float middleY = mapHeight / 2;
         for (int y = 0; y < mapHeight; y++)
         {
             for(int x = 0; x < mapWidth; x++)
@@ -20,8 +33,8 @@ public static class Noise
                 for (int o = 0; o < octaves; o++)
                 {
                     //calculate the current x and y values (the higher the freq, the further the points)
-                    float x_coord = x / scale * frequency;
-                    float y_coord = y / scale * frequency;
+                    float x_coord = (x-middleX) / scale * frequency + octaveOffsets[o].x;
+                    float y_coord = (y-middleY) / scale * frequency + octaveOffsets[o].y;
 
                     //calculate the perlin value for the current x,y
                     float perlVal = Mathf.PerlinNoise(x_coord,y_coord) * 2 - 1; // (*2-1 allows for negative values)
